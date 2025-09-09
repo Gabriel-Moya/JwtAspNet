5c9100bb9af9b8b8728f3f957507101b41eeee48
Gabriel Moya <gabrielmoya123@gmail.com>
2025-09-08 20:33:19 -0400

    feat: add configuration class

---
+++ Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace JwtAspNet;
+
+public static class Configuration
+{
+    // Only for study purposes. Do NOT use this key in production.
+    private static string PrivateKey { get; set; } = "your-256-bit()*&^%^^$#-secret-your-256-bit-secret!@#$%^";
+}
