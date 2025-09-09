88ac21f06241a874e069c2576b3c7b11a41bb4fd
Gabriel Moya <gabrielmoya123@gmail.com>
2025-09-09 11:09:44 -0400

    Implement JWT authentication and update endpoints

--- Program.cs
+++ Program.cs
@@ -1,23 +1,45 @@
+using JwtAspNet;
 using JwtAspNet.Models;
 using JwtAspNet.Services;
-using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<AuthService>();
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.PrivateKey)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/", (AuthService service) => {
+app.MapGet("/login", (AuthService service) => {
     var user = new User(
         1,
         "Gabriel Moya",
         "example@email.com",
         "https://exampleimage.com/image.jpg",
         "supersecretPassword",
-        new[] { "student", "premium" });
+        ["student", "premium"]);
 
     return service.CreateToken(user);
 });
+
+app.MapGet("/restrito", () => "Access Granted").RequireAuthorization();
 
 app.Run();
